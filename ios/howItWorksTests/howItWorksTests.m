b4dc9960c58c11bcba48ebf96280a1e15d964383
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-09-16 09:21:59 +0200

    Reinitialize project with another configuration

--- ios/howItWorksTests/howItWorksTests.m
+++ ios/howItWorksTests/howItWorksTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyAppTests : XCTestCase
+@interface howItWorksTests : XCTestCase
 
 @end
 
-@implementation MyAppTests
+@implementation howItWorksTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,28 +1,26 @@
 import React, {FC, useState} from 'react';
 import styles from './styles';
-import {ICounterProps} from './types';
 import {Text, TouchableOpacity, View} from 'react-native';
 
-const Counter: FC<ICounterProps> = ({description}) => {
-  const [count, setCount] = useState(0);
+interface ICounterProps {
+  description: string;
+}
 
+const Counter: FC<ICounterProps> = ({description}) => {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        onPress={() => setCount(count - 1)}
-        style={styles.button}>
-        <Text style={styles.buttonText}>decrement</Text>
+        style={styles.button}
+        onPress={() => setCounter(counter + 1)}>
+        <Text>increment</Text>
       </TouchableOpacity>
 
-      <Text>
-        {description}: {count}
-      </Text>
-
-      <TouchableOpacity
-        onPress={() => setCount(count + 1)}
-        style={styles.button}>
-        <Text style={styles.buttonText}>increment</Text>
-      </TouchableOpacity>
+      <View>
+        <Text>
+          {description}: {counter}
+        </Text>
+      </View>
     </View>
   );
 };
